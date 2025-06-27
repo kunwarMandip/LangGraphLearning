73cd5949648ce76ea3a16285f1ea39822c3d6294
Kunwarm <78311053+kunwarMandip@users.noreply.github.com>
2025-06-27 11:06:53 +0100

    fixed recursion issue

--- app/dynamic_groq_weather_agent.py
+++ app/dynamic_groq_weather_agent.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+
 from langchain_groq import ChatGroq
+from langchain_core.tools import tool
 from langchain_core.messages import AnyMessage
 from langchain_core.runnables import RunnableConfig
 from langgraph.prebuilt.chat_agent_executor import AgentState
@@ -15,8 +17,8 @@ def get_weather(location: str) -> str:
     return f"Its always sunny in {location}!"
 
 def prompt(state: AgentState, config: RunnableConfig) -> list[AnyMessage]:
-    user_name = config["configurable"].get("username")
-    system_message = f"You are a helpful assistant. Address the user as {user_name}"
+    user_name = config["configurable"].get("user_name")
+    system_message = f"You are a helpful assistant. Address the user as: {user_name}"
     return [{"role": "system", "content": system_message}] + state["messages"]
 
 agent = create_react_agent(
@@ -25,8 +27,12 @@ agent = create_react_agent(
     prompt = prompt
 )
 
-results = agent.invoke(
-    {"messages": [{"role": "user", "content": "What is the weather in london?"}]}
+results =  agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]},
+    config={
+        "configurable": {"user_name": "John Smith"},
+        "recursion_limit": 20
+    },
 )
 
 print (results)
